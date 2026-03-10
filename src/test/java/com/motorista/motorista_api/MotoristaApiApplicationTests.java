35e957aedc3b9bb488d1f528a255c0b705cc6709
Lucas Daniel Souza Dias <lucas.dias@viacaocometa.com.br>
2026-03-10 14:19:15 -0300

    feat: inicializa backend do projeto motorista com configuração do Spring Boot e conexão com banco

---
+++ src/test/java/com/motorista/motorista_api/MotoristaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorista.motorista_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoristaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
